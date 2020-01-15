ba0f5e2d0a1145e60722233554f824a6601434ea
Victoria Montero <geekvico@gmail.com>
2020-01-15 22:05:18 +0100

    4 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TranslateFlagsComponent } from './translate-flags/translate-flags.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TranslateFlagsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/translate-flags/translate-flags.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-translate-flags',
+  templateUrl: './translate-flags.component.html',
+  styleUrls: ['./translate-flags.component.scss']
+})
+export class TranslateFlagsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
